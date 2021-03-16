f3711ceb653789a8b5bb551603722eec5021854a
rodrigobentorodrigues <rodrigobentor2014@gmail.com>
2021-03-15 22:16:32 -0300

    Added and configured^Cpenapi to project

--- src/main/java/br/com/starwars/starwarsapi/controllers/PlanetResource.java
+++ src/main/java/br/com/starwars/starwarsapi/controllers/PlanetResource.java
@@ -3,6 +3,11 @@ package br.com.starwars.starwarsapi.controllers;
 import br.com.starwars.starwarsapi.models.Planet;
 import br.com.starwars.starwarsapi.services.PlanetAPIService;
 import br.com.starwars.starwarsapi.services.PlanetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,11 +36,22 @@ public class PlanetResource {
     private PlanetAPIService planetAPIService;
 
     @GetMapping
+    @Operation(summary = "Get a list of planets")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "List of planets",
+                content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Planet.class))})})
     public ResponseEntity<List<Planet>> get() {
         return new ResponseEntity<List<Planet>>(planetService.listAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a planet by its id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the planet",
+                content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Planet.class))}),
+        @ApiResponse(responseCode = "404", description = "Planet not found", content = @Content)})
     public ResponseEntity<Planet> getByID(@PathVariable("id") String id) {
         Planet planetByID = planetService.getByID(id);
         if (planetByID != null) {
@@ -46,6 +62,12 @@ public class PlanetResource {
     }
 
     @GetMapping("/name/{name}")
+    @Operation(summary = "Get a planet by its name")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Found the planet",
+                content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Planet.class))}),
+        @ApiResponse(responseCode = "404", description = "Planet not found", content = @Content)})
     public ResponseEntity<Planet> getByName(@PathVariable("name") String name) {
         Planet planetByID = planetService.getByName(name);
         if (planetByID != null) {
@@ -56,6 +78,12 @@ public class PlanetResource {
     }
 
     @PostMapping
+    @Operation(summary = "Insert a planet")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Insert the planet",
+                content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Planet.class))}),
+        @ApiResponse(responseCode = "400", description = "Invalid properties supplied", content = @Content)})
     public ResponseEntity<Planet> post(@Valid @RequestBody Planet planet) {
         int numberOfOcurrencesInFilms = planetAPIService.numberOfFilmsByPlanet(planet.getName());
         planet.setNumberOfFilmsAppearances(numberOfOcurrencesInFilms);
@@ -64,6 +92,12 @@ public class PlanetResource {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a planet by its id")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Delete successuful",
+                content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Planet.class))}),
+        @ApiResponse(responseCode = "404", description = "Planet not found", content = @Content)})
     public ResponseEntity<Void> delete(@PathVariable("id") String id) {
         Planet planet = planetService.getByID(id);
         if (planet != null) {
